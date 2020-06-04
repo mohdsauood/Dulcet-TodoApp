b849d1dc5e74936348fb2465de77981e2f589b38
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-06-04 18:40:58 +0400

    made the required homepage content dynamically generated from js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 // datefns library
 import {lightFormat} from 'date-fns'
+import {generateHomepageContent} from './dynamicHomePage.js'
+// var result = parse('23 march 6am', 'd MMMM ha', new Date());
+// console.log(result);
 
-var result = parse('23 march 6am', 'd MMMM ha', new Date());
-console.log(result);
+
+
+window.addEventListener('load',()=>{
+generateHomepageContent();
+});
