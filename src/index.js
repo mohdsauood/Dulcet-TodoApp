85bf2b1df5e753553ea7efaae038aa6f73245cf1
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-06-04 18:02:29 +0400

    added basic styling and elements for viewing task page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 // datefns library
 import {lightFormat} from 'date-fns'
 
-var result = parse('23 march 6am', 'd MMMM ha', new Date());
-console.log(result);
+// var result = parse('23 march 6am', 'd MMMM ha', new Date());
+// console.log(result);
+
+
+//testing text area content generation
+const textarea=document.querySelector('#description');
+textarea.textContent="rererlorem    ";
