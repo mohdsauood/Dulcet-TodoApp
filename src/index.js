7c26f46e4b6ad63e31f5ba77b29c8c5284d1fee0
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-06-07 02:09:54 +0400

    error checking logic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 // datefns library
 import {
     lightFormat,
-    parse
+    parse,
+    isPast
 } from 'date-fns'
 
 import{
+setError,clearError
+} from './errorHandlers.js'
 
-} from './taskValidation.js'
-// var result = parse('23 march 6am', 'd MMMM ha', new Date());
+import{
+    eventsForAddTaskPage
+} from './addTaskpageEventListeners.js'
+
+
+const backButtonEvent=()=>{
+    checkError();
+console.log('saiyo nara oni chan');
+}
+
+const checkError=(e)=>{
+    const dateElement=document.querySelector('#duedate');
+    clearError();
+    let dateValue=parse(dateElement.value,'yyyy-MM-dd',new Date());
+    if(isPast(dateValue))
+    {
+        e.preventDefault();
+        setError('Due date already passed ,set new date');
+    }
+    else if(x){
+        //make backbutton a form submit button
+        //check if required etc etc
+    }
+}
+
+eventsForAddTaskPage(checkError);
+
+const backButton=document.querySelector('.backButton');
+backButton.addEventListener('click',backButtonEvent);
+
