74eef2779f02c84e5f13d1b53b30ea35ef5efa71
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-06-07 14:34:19 +0400

    hid checkError implementation from indexpage

--- src/errorHandlers.js
+++ src/errorHandlers.js
@@ -9,7 +9,21 @@ const clearError=()=>{
     span.textContent="";
 }
 
+const checkError=(e,parse,isPast)=>{
+    const dateElement=document.querySelector('#duedate');
+    clearError();
+    let dateValue=parse(dateElement.value,'yyyy-MM-dd',new Date());
+    if(isPast(dateValue))
+    {
+        e.preventDefault();
+        setError('Due date already passed ,set new date');
+    }
+    else if(x){
+        //make backbutton a form submit button
+        //check if required etc etc
+    }
+}
 
 
 
-export {setError,clearError}
+export {setError,clearError,checkError}
