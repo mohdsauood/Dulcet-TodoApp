0669eeef85bd70bcad5ad79f965904bdacff4b48
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-06-07 14:34:49 +0400

    cleaned up and created individual modules

--- src/index.js
+++ src/index.js
@@ -6,36 +6,14 @@ import {
 } from 'date-fns'
 
 import{
-setError,clearError
+setError,clearError,checkError
 } from './errorHandlers.js'
 
 import{
-    eventsForAddTaskPage
+    dateEventForAddTaskPage
 } from './addTaskpageEventListeners.js'
 
 
-const backButtonEvent=()=>{
-    checkError();
-console.log('saiyo nara oni chan');
-}
 
-const checkError=(e)=>{
-    const dateElement=document.querySelector('#duedate');
-    clearError();
-    let dateValue=parse(dateElement.value,'yyyy-MM-dd',new Date());
-    if(isPast(dateValue))
-    {
-        e.preventDefault();
-        setError('Due date already passed ,set new date');
-    }
-    else if(x){
-        //make backbutton a form submit button
-        //check if required etc etc
-    }
-}
-
-eventsForAddTaskPage(checkError);
-
-const backButton=document.querySelector('.backButton');
-backButton.addEventListener('click',backButtonEvent);
+dateEventForAddTaskPage(checkError,parse,isPast);
 
