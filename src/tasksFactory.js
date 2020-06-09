6ad1c500669e186771597c9c8d81646072cf3b18
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-06-10 01:21:28 +0400

    Update tasksFactory.js

--- src/tasksFactory.js
+++ src/tasksFactory.js
@@ -1,10 +1,8 @@
-import{
-    testman
-} from './quotesGenerator.js'
+
 
 const titleElement=document.querySelector('#title');
 const descriptionElement=document.querySelector('#description');
-testman();
+
 
 //import is working 
 // so clear the complexity and reduce the imports and passing the functions as parameteres except the datefns one as they require bundling and only be used my index js. cut and chop the rest of the parameters and provided access to everybody and make the code clean and use imports everywhere 
