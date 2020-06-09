7fec0ccb3dcad2fe9c62fb30822eb3f2e65117c8
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-06-09 17:13:54 +0400

    Create errorHandlers.js

---
+++ src/errorHandlers.js
@@ -0,0 +1,26 @@
+const setError=(text)=>{
+    const span=document.querySelector('#errorSpan');
+    span.textContent=text;
+}
+const clearError=()=>{
+    const span=document.querySelector('#errorSpan');
+    span.textContent="";
+}
+
+const checkError=(event,parse,isPast)=>{
+    const dateElement=document.querySelector('#duedate');
+    clearError();
+    let dateValue=parse(dateElement.value,'yyyy-MM-dd',new Date());
+    if(isPast(dateValue))
+    {
+        event.preventDefault();
+        setError('Due date already passed ,set new date');
+        return false;
+    }
+    return true;
+    
+}
+
+
+
+export {checkError}
