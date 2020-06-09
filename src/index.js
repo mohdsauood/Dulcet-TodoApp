e765ea4a5eab2593a7fee0d46a8d037cf8defedd
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-06-10 01:22:16 +0400

    moved everything into its modules

--- src/index.js
+++ src/index.js
@@ -9,56 +9,19 @@ import {
     generateHomepageContent
 } from './dynamicHomePage.js'
 import {
-    generateAddTaskContent
-} from './addTaskPage.js'
+    generateHomepageEvents
+} from './homePageEventListeners.js'
 import {
-    generateViewTaskpageContent
-} from './viewTaskPage.js'
-import {
-    setQuote
+    setQuote,setCurrentDate
 } from './quotesGenerator.js'
 import {
     checkError
 } from './errorHandlers.js'
 
-import {
-    generateAddTaskPageEvents
-} from './addTaskpageEventListeners.js'
-
-
-// Event listeners for home page
-
-const addButtonEvent = () => {
-    const addButton = document.querySelector('#addButton');
-    addButton.addEventListener('click', () => {
-        generateAddTaskContent();
-        generateAddTaskPageEvents(checkError,parse,isPast,generateHomepageContent,addHomePageEvents);
-    });
-}
-const viewTaskButtonEvent = () => {
-    const tasks = document.querySelectorAll('#task');
-    tasks.forEach((item) => {
-        item.addEventListener('click', () => {
-            generateViewTaskpageContent();
-            backButtonEvent();
-        });
-    })
-}
-const addHomePageEvents = () => {
-    addButtonEvent();
-    viewTaskButtonEvent();
-}
-
-//setting date everyday
-
-const setCurrentDate = () => {
-    const dateElement = document.querySelector('#homePageDate');
-    dateElement.textContent = format(new Date(), 'do MMMM yyy');
-}
 
 
 
 setQuote(format(new Date(), 'eee'));
 setCurrentDate();
 generateHomepageContent();
-addHomePageEvents();
+generateHomepageEvents(parse,isPast);
