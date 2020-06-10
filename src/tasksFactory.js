0b1342ec86503dd4a15ae38439a8dea61040133d
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-06-10 23:03:40 +0400

    task object is created sucessfully

--- src/tasksFactory.js
+++ src/tasksFactory.js
@@ -1,5 +1,10 @@
-import { checkError } from "./errorHandlers";
-import {format,parse} from './index.js';
+import {
+    checkError
+} from "./errorHandlers";
+import {
+    format,
+    parse
+} from './index.js';
 
 
 //create a createtask function that has check condition then create obj then set it in local storage
@@ -7,69 +12,90 @@ import {format,parse} from './index.js';
 //,then call display
 
 
-const checkTasksCondition=()=>{
-    const titleElement=document.querySelector('#title');
-    const descriptionElement=document.querySelector('#description');
-    if(titleElement.value!=null ||descriptionElement.value!=null)
-    {
+const checkTasksCondition = () => {
+    const titleElement = document.querySelector('#title');
+    const descriptionElement = document.querySelector('#description');
+    if (titleElement.value != null || descriptionElement.value != null) {
         return true;
     }
     return false;
 }
 
-const taskFactory=(timeCreatedinMillis,{timeCreated,priority,duedate,title,descritpion})=>{
+const taskFactory = ({
+    timeCreated,
+    priority,
+    mainDueDate,
+    title,
+    descritpion
+} = {}) => {
 
-    return{
-        timeCreatedinMillis:{
-                                timeCreated,
-                                priority,
-                                duedate,
-                                tiltle,
-                                descritpion
-                            },
+    return {
+        timeCreated,
+        priority,
+        mainDueDate,
+        title,
+        descritpion,
+        }
     }
 
-}
 
-const createTask=()=>{
-    if(checkTasksCondition)
-    {
-        const priorityElement=document.querySelector('#priority');
-        const duedateElement=document.querySelector('#duedate');
-        const hourElement=document.querySelector('#hour');
-        const minuteElement=document.querySelector('#minute');
-        const ampmElement=document.querySelector('#ampm');
-        const titleElement=document.querySelector('#title');
-        const descriptionElement=document.querySelector('#description');
-        let currentDate=format(new Date(), 'MMMMd');
-        let timeCreatedinMillis=format(new Date(),'T');
-        let timeCreated=format(new Date(),'h:mm a');
-        let priority=priorityElement.value;
-        let dueDate=duedateElement.value;
-        let dueHour=hourElement.value;
-        let dueMinute=minuteElement.value;
-        let dueAmPm=ampmElement.value;
-        let mainDueDate=`${dueDate} ${dueHour}:${dueMinute} ${dueAmPm}`;
-        let title=titleElement.value;
-        let descritpion=descriptionElement.value;
-        
-        let testObj={currentDate,timeCreatedinMillis,timeCreated,priority,mainDueDate,title,descritpion}
-        console.log(testObj);
-        // const tasksObject={};
-        // const tasksObject[currentDate]=taskFactory(timeCreatedinMillis,{timeCreated:timeCreated,priority:priority,duedate:duedate,duetime:duetime,title:title,descritpion:descritpion})
+
+const createTask = () => {
+    if (checkTasksCondition) {
+        const priorityElement = document.querySelector('#priority');
+        const duedateElement = document.querySelector('#duedate');
+        const hourElement = document.querySelector('#hour');
+        const minuteElement = document.querySelector('#minute');
+        const ampmElement = document.querySelector('#ampm');
+        const titleElement = document.querySelector('#title');
+        const descriptionElement = document.querySelector('#description');
+        let currentDate = format(new Date(), 'MMMMd');
+        let timeCreatedinMillis = format(new Date(), 'T');
+        let timeCreated = format(new Date(), 'h:mm a');
+        let priority = priorityElement.value;
+        let dueDate = duedateElement.value;
+        let dueHour = hourElement.value;
+        let dueMinute = minuteElement.value;
+        let dueAmPm = ampmElement.value;
+        let mainDueDate = `${dueDate} ${dueHour}:${dueMinute} ${dueAmPm}`;
+        let title = titleElement.value;
+        let descritpion = descriptionElement.value;
+
+        // let testObj = {
+        //     currentDate,
+        //     timeCreatedinMillis,
+        //     timeCreated,
+        //     priority,
+        //     mainDueDate,
+        //     title,
+        //     descritpion
+        // }
+        // console.log(testObj);
+        const tasksObject = {};
+        tasksObject[currentDate]={};
+        tasksObject[currentDate][timeCreatedinMillis] = taskFactory({
+            timeCreated,
+            priority,
+            mainDueDate,
+            title,
+            descritpion
+        });
+        console.log(tasksObject);
     }
 }
 
 // let mainObject={
-    //  march25:{
-    //     milliseconds:{
-    //                     timeCreated:'time',
-    //                     priority:'low',
-    //                     duedate:'egegegege',
-    //                     title:'sleeping',
-    //                     descritpion:'etetetet',
-    //                  },
-    //          },
-    // }
+//  march25:{
+//     milliseconds:{
+//                     timeCreated:'time',
+//                     priority:'low',
+//                     duedate:'egegegege',
+//                     title:'sleeping',
+//                     descritpion:'etetetet',
+//                  },
+//          },
+// }
 
-export { createTask}
+export {
+    createTask
+}
