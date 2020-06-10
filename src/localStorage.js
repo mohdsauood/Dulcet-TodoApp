8745b0de047e5dc4464472b1b3cafa5a61aeb883
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-06-11 02:00:55 +0400

    cleaned tasksFactory.js

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,4 +1,5 @@
 const setToLocalStorage=(obj)=>{
+    console.log('yeah yeah check function diddnt work');
     localStorage.setItem('tasks',JSON.stringify(obj));
 }
 
--- src/tasksFactory.js
+++ src/tasksFactory.js
@@ -13,11 +13,31 @@ import {
 //set object in local storage
 //,then call display
 
-
+const getElements=()=>{
+    const tasksObject = (localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : {});
+        const priorityElement = document.querySelector('#priority');
+        const duedateElement = document.querySelector('#duedate');
+        const hourElement = document.querySelector('#hour');
+        const minuteElement = document.querySelector('#minute');
+        const ampmElement = document.querySelector('#ampm');
+        const titleElement = document.querySelector('#title');
+        const descriptionElement = document.querySelector('#description');
+        let currentDate = format(new Date(), 'MMMMd');
+        let timeCreatedinMillis = format(new Date(), 'T');
+        let timeCreated = format(new Date(), 'h:mm a');
+        let priority = priorityElement.value;
+        let dueDate = duedateElement.value;
+        let dueHour = hourElement.value;
+        let dueMinute = minuteElement.value;
+        let dueAmPm = ampmElement.value;
+        let mainDueDate = `${dueDate} ${dueHour}:${dueMinute} ${dueAmPm}`;
+        let title = titleElement.value;
+        let descritpion = descriptionElement.value;
+}
 const checkTasksCondition = () => {
     const titleElement = document.querySelector('#title');
     const descriptionElement = document.querySelector('#description');
-    if (titleElement.value != null || descriptionElement.value != null) {
+    if (titleElement.value != "" || descriptionElement.value != "") {
         return true;
     }
     return false;
@@ -43,26 +63,8 @@ const taskFactory = ({
 
 
 const createTask = () => {
-    if (checkTasksCondition) {
-        const tasksObject = (localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : {});
-        const priorityElement = document.querySelector('#priority');
-        const duedateElement = document.querySelector('#duedate');
-        const hourElement = document.querySelector('#hour');
-        const minuteElement = document.querySelector('#minute');
-        const ampmElement = document.querySelector('#ampm');
-        const titleElement = document.querySelector('#title');
-        const descriptionElement = document.querySelector('#description');
-        let currentDate = format(new Date(), 'MMMMd');
-        let timeCreatedinMillis = format(new Date(), 'T');
-        let timeCreated = format(new Date(), 'h:mm a');
-        let priority = priorityElement.value;
-        let dueDate = duedateElement.value;
-        let dueHour = hourElement.value;
-        let dueMinute = minuteElement.value;
-        let dueAmPm = ampmElement.value;
-        let mainDueDate = `${dueDate} ${dueHour}:${dueMinute} ${dueAmPm}`;
-        let title = titleElement.value;
-        let descritpion = descriptionElement.value;
+    if (checkTasksCondition()) {
+        getElements();
 
         if(tasksObject[currentDate])
         {
@@ -84,8 +86,6 @@ const createTask = () => {
                 descritpion
             });
         }
-        
-        console.log(tasksObject);
         setToLocalStorage(tasksObject);
     }
 }
