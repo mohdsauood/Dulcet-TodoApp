265514bf6df8d2b11cc90ef56a07b7847f172714
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-06-13 02:39:08 +0400

    added comments

--- src/index.js
+++ src/index.js
@@ -21,8 +21,8 @@ import {
 //add feature when hovered over circle show the priority and its meaning
 //on clearlist make an absolute position middle of screen button , to confirm delete all ! if yes then delete all .
 //see wordpress or shopify tuts to create a food order app
-//now i'll see if anything is edited on view page then set the changes to the object and go back
 //somehow make the maindue date into march 1st 2nd etc
+//fix the logo
 
 setQuote(format(new Date(), 'eee'));
 setCurrentDate(format);
