e5ac266d4b709f838e5cdf662c525364fb2551db
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-06-17 17:02:42 +0400

    increased text limit for title

--- src/addTaskPage.js
+++ src/addTaskPage.js
@@ -43,7 +43,7 @@ const generateAddTaskContent = () => {
     <option value="PM">PM</option>
 </select>
     <br>
-    <input type="text" placeholder="title" id="title"  maxlength="20" >
+    <input type="text" placeholder="title" id="title"  maxlength="100" >
     <br>
     <textarea placeholder="description" id="description" cols="30" rows="10"></textarea>
    <br>
